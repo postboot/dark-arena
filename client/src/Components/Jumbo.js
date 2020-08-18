51b132d13e3f8adea4156347447a1111080bc465
Ken Williams <kenwilliams74@gmail.com>
2020-08-18 13:51:58 -0700

    Almost done!

---
+++ client/src/Components/Jumbo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Jumbo() {
+
+
+    return (
+    <>
+        <div className="jumbotron">
+            <h1 className="display-4">Hello Future Champion</h1>
+            <p className="lead">Are you ready for battle?</p>
+        </div>
+    </>
+
+    )
+};
+
+export default Jumbo;
---
+++ client/src/Components/NoButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function NoButton() {
+
+
+    return (
+        <div className = "container">
+            <p>
+        <button type="button" className="btn btn-danger btn-lg">No I have to use the restroom</button>
+        </p>
+    </div>
+
+    )
+};
+
+export default NoButton;
